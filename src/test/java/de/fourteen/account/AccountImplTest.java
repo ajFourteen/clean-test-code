65097b4fbf2e98028ed8068cf8a237ee804f1641
ajFourteen <andreas.juergensen@fourteen-consulting.com>
2025-09-29 17:25:23 +0200

    given when then

--- src/test/java/de/fourteen/account/AccountImplTest.java
+++ src/test/java/de/fourteen/account/AccountImplTest.java
@@ -3,6 +3,7 @@ package de.fourteen.account;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountImplTest {
@@ -11,11 +12,26 @@ class AccountImplTest {
 
   @Test
   void test1() {
+    // given
+
+    // when
     sut.deposit(100);
+
+    // then
     if (sut.balance() != 100) {
       fail("Balance not correct");
     }
+  }
+
+  @Test
+  void test1a() {
+    // given
+    sut.deposit(100);
+
+    // when
     sut.withdraw(50);
+
+    // then
     if (sut.balance() != 50) {
       fail("Balance not correct after withdraw");
     }
@@ -23,20 +39,27 @@ class AccountImplTest {
 
   @Test
   void test2() {
+    // given
     sut.deposit(50);
-    try {
-      sut.withdraw(100);
-      fail("Expected exception not thrown");
-    } catch (RuntimeException e) {
-      assertThat(e.getMessage()).contains("funds");
-    }
+
+    // when
+    var exception = catchThrowable(() -> sut.withdraw(100));
+
+    // then
+    assertThat(exception).isNotNull();
+    assertThat(exception.getMessage()).contains("funds");
   }
 
   @Test
   void test3() {
+    // given
     Account another = new AccountImpl();
     sut.deposit(200);
+
+    // when
     sut.transfer(another, 50);
+
+    // then
     assertEquals(150, sut.balance());
     assertEquals(50, another.balance());
   }
